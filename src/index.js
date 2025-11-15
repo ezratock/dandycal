0164024e7cd9c132f2a9105509ee5cba65b95b7f
Skylar Abruzese <skylar@abruzese.com>
2025-11-14 22:23:09 -0500

    add testing for gcal integration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+import { createCalendarUrlFromImage } from "./lib/cal-url-from-image.js";
+
+const imagePath = process.argv[2];
+
+if (!imagePath) {
+	console.error("Usage: npm start -- <image-path>");
+	process.exit(1);
+}
+
+createCalendarUrlFromImage(imagePath)
+	.then(({ event, url }) => {
+		console.log("Structured event JSON:");
+		console.log(JSON.stringify(event, null, 2));
+		console.log("\nGoogle Calendar URL:");
+		console.log(url);
+	})
+	.catch((err) => {
+		console.error("Error:", err);
+		process.exit(1);
+	});
